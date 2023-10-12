1f19aaab7d73168d32f271089c91ed07c7837122
Sanjay2N <114469456+Sanjay2N@users.noreply.github.com>
2023-10-12 12:28:32 +0530

    Add files via upload

--- Expressintro.js
+++ Expressintro.js
@@ -1,22 +1,33 @@
 const http=require('http');
 const express=require("express");
+const bodyparser=require('body-parser')
+
+
 
 const app=express();
-const rout=require('./routes');
+app.use(bodyparser.urlencoded({extended:false}))
 
-app.use((req,res,next)=>{
-    console.log("in the middleware");
-    next();//allows to request tot continue to next middleware in line
-});
-app.use((req,res,next)=>{
-    console.log("in the middleware 2")
-    // res.setHeader('Content-type','text/html')
-    // res.send("<h1>hello</h1>");
-    res.send( { key1: 2 })
+app.use('/add-product',(req,res,next)=>{
+    res.send('<html><form action="/product",method="POST"> \
+    <input type="text" name="title" id=""> \
+        <button type="submit">submit</button>\
+</form>\
+</html>');
+    
 })
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+    
+});
+
+app.use('/',(req,res,next)=>{
+    res.send("<h1>in the url /</h1>");//allows to request tot continue to next middleware in line
+});
 
 // const server=http.createServer(rout.handler);
 // const server=http.createServer(app);
 // server.listen(400);
 
-app.listen(4);
+app.listen(500);
